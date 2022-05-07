a7390850c50ac94eb7cd7ad03e74d77d8a7ce056
Gabriela Lara de Souza <gaabi_lara@hotmail.com>
2022-05-06 23:20:48 -0300

    [DEV][V.1] Lista de exercicios Warren Ex 1 e 2
    Criados as aplicações das atividades 1 e 2 da lista disponibilizada.

---
+++ WarrenDesafio1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace WarrenDesafio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limite = 1000000; // Configura limite que deseja validar
+            int numeroInvertido = 0; // Variavel para armazenar o numero invertido
+            int soma = 0; // Variavel que armazena o valor total N + reverso(n)
+            //int contaNumero = 0; //Variavel para apresentar quantidade de registros (somente execução teste)
+            
+
+            Console.WriteLine("Retorna os numeros \"invertido\" + ele mesmo que resulta em um valor impar: ");
+
+            for (int j = 0; j <= limite; j++) {
+                numeroInvertido = retornaInvertido(j); // Preenche valor do numero reverso com função
+                if (numeroInvertido > 0)
+                {
+                    soma = j + numeroInvertido; // Soma N + reverso(n)
+                    if (((soma % 2) != 0) && (soma <= limite)) // Avalia se a soma dos numeros é IMPAR 
+                    {                        
+                        Console.WriteLine(j.ToString()); // Retorna os numeros reversiveis entre 0 e o limite configurado acima
+                        /*Console.WriteLine(contaNumero++.ToString() + ": "
+                            + j.ToString()
+                            + " + "
+                            + numeroInvertido.ToString()
+                            + " = "
+                            + soma.ToString());*/ // este comando serve para apresentar na tela a soma e quantidade de numeros
+                    }
+                }
+            }
+
+        }
+
+        static public int retornaInvertido(int numero) {
+
+            string auxNumero = numero.ToString();
+            string numeroInvertido = "";
+
+            // Nesta função transformo o numero em uma string e incremento ela somando o caractere em questão + a string anterior
+            for (int i = 0; i < auxNumero.Length; i++){
+                numeroInvertido = auxNumero.Substring(i, 1) + numeroInvertido;
+            };
+            if (numeroInvertido.Substring(0, 1) == "0")
+            {
+                // Validação para ignorar numeros que começam com '0'
+                return 0;
+            } else {
+                // Retorna o numero invertido
+                return Int32.Parse(numeroInvertido);
+            }   
+        }
+
+
+    }
+}
---
+++ WarrenDesafio2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace WarrenDesafio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalAlunos = 0; // Inicia Total de alunos
+            int limiteAtraso = 0; // Inicia limite de atraso
+            
+            Console.WriteLine("Olá Professor! \nInforme a quantidade de alunos da sua aula: ");
+            totalAlunos = Int32.Parse(Console.ReadLine());
+            int[] alunos = new int[totalAlunos]; // Cria Array para armazenar os horarios de chegada de cada aluno
+
+            Console.WriteLine("Agora informe o limite de atraso para seguir com a aula: ");
+            limiteAtraso = Int32.Parse(Console.ReadLine()); // Informa o limite de atraso para suspensão da aula
+            
+
+            Console.Clear(); // Limpa Console, abaixo gerei um cabeçalho para facilitar leitura
+            Console.WriteLine("Total de Alunos : " + totalAlunos.ToString() + "\n"
+                            + "Limite de atrasos: " + limiteAtraso.ToString());
+            Console.WriteLine("\n\nPara marcar as presenças dos alunos use números inteiros,\n" +
+                "sendo eles menores que zero e zero para no HORARIO e maiores que 0 para ATRASADOS.");
+            Console.WriteLine("--------------------------------------------------------");
+            for (int i = 0; i < totalAlunos; i++) { // Preenche os horários de chegada de cada aluno
+                Console.WriteLine("Aluno " + (i+1).ToString() + " chegou, marque seu horario: ");
+                alunos[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.Clear(); // Limpa e gera cabeçalho
+            Console.WriteLine("Total de Alunos : " + totalAlunos.ToString() + "\n"
+                            + "Limite de atrasos: " + limiteAtraso.ToString());
+            Console.WriteLine("--------------------------------------------------------");
+
+            for (int i = 0; i < alunos.Length; i++) {
+                if (alunos[i] > 0) { // verifica se o aluno está atrasado
+                    limiteAtraso--; // Reduz total do limite
+                }
+            }
+
+            if (limiteAtraso < 0) { // Valida se o limite foi passado ou não 
+                Console.WriteLine("A Aula foi Suspensa!"); // Caso passou, o limite fica abaixo de 0, indicando suspensao da aula
+            } else {
+                Console.WriteLine("Aula Normal!");// Caso não alcançe o limite, fica acima de 0, indicando aula normal
+            }
+
+        }
+    }
+}
